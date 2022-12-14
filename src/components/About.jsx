f85d97598946a719fe4b8c503b14bea7161ebd46
Tamirchn1@gmail.com <T2151991t>
2022-12-14 19:38:39 +0100

    about artist links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,18 +14,42 @@ function About() {
         <p className="about-txt">
           I am Tamir, I play on various instruments but mostly bass guitar. I
           played/recorded with - <br />
-          Red Axes, Moscoman, Soda Fabric, Autarkic, Chicos de nazca, Häxxan,
-          Cherry bandora, Avishag cohen rodrigues, Neta Polturak , Paul arambula
+          <a href="https://www.facebook.com/redaxesmusic">Red Axes</a> ,
+          <a href="https://www.facebook.com/moscoman">Moscoman</a> ,{""}
+          <a href="https://www.facebook.com/search/top?q=soda%20fabric">
+            Soda Fabric ,
+          </a>
+          <a href="https://www.facebook.com/AutarkicAutarkic">Autarkic</a> ,
+          <a href="https://www.facebook.com/profile.php?id=100059272549565">
+            Chicos de nazca
+          </a>{" "}
+          ,<a href="https://www.facebook.com/xhaxxanx">Häxxan</a> ,
+          <a href="https://www.facebook.com/hopacherrybandora">
+            Cherry bandora
+          </a>{" "}
+          ,
+          <a href="https://www.facebook.com/THEHOUSEOFLIGHT">House of light </a>
+          ,
+          <a href="https://avishagcrodrigues.bandcamp.com/?fbclid=IwAR26DTKDoyOoJW86IBVkc8hGDNL35oLoTbRxjMhJY3uA8mRUPwWasMH0qkY">
+            Avishag cohen rodrigues
+          </a>{" "}
+          ,<a href="https://www.facebook.com/ok.aleklee">Alek Lee</a> ,
+          <a href="https://www.facebook.com/NetaPolturak">Neta Polturak</a> ,
+          <a href="https://soundcloud.com/paularambula?fbclid=IwAR1QvqSA9RpP-h2YBlbU-Eufkxs8v7Txw6mvQT5fn8FzTLRc5lkUPnleBHg">
+            Paul arambula
+          </a>{" "}
           and more...
           <br />
           <br />
           In the past years, alongside touring across Europe, I wrote music for
           theater, art exhibition and a video game, as well produced dubbing art
-          for various projects. I've founded 'Solarium', events and content
-          production in Berlin, and Maakuda record label. I've produced other
-          musicians in their musical quest to their album. I also teach Bass
-          guitar and Ableton Live software in my place in Neukölln or remote via
-          Skype.
+          for various projects. I've founded{" "}
+          <a href="https://www.facebook.com/solariumcrew">'Solarium'</a> ,
+          events and content production in Berlin, and{" "}
+          <a href="https://maakudarecords.com/"> 'Maakuda record'</a> label.
+          I've produced other musicians in their musical quest to their album. I
+          also teach Bass guitar and Ableton Live software in my place in
+          Neukölln or remote via Skype.
         </p>
       </div>
       <Footer />
